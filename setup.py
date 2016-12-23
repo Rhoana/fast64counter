6f38c74405912e16e5d6362c5e4f9599ec946fe8
Thouis (Ray) Jones <thouis@gmail.com>
2016-12-23 12:21:23 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Distutils import build_ext
 import numpy
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("fast64counter", ["fast64counter.pyx", "khash.pxd"])],
-    include_dirs = [numpy.get_include(),"./klib"],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension("fast64counter", ["fast64counter.pyx", "khash.pxd"],
+                           include_dirs=[numpy.get_include(), "./klib"])]
 )
