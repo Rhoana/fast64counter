75618c2f3d31dc7d530523cae43fa03230972d6f
Lee Kamentsky <lee_kamentsky@g.harvard.edu>
2017-02-03 16:14:32 -0500

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from Cython.Distutils import build_ext
 import numpy
 
 setup(
+    name='rh_fast64counter',
+    version="1.0.0",
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("fast64counter", ["fast64counter.pyx", "khash.pxd"])],
     include_dirs = [numpy.get_include(),"./klib"],
